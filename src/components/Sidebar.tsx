95d17900398a936b764e57a9b14f803bd75242b3
ezamb <ezamb@example.com>
2026-01-09 09:28:57 +0300

    Fix: Sidebar useRouter import eksikligi duzeltildi

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { 
   Home as HomeIcon, 
   Package as PackageIcon, 
